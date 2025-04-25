1df2d3887b43169378b4fc86f6bc75639a05ad2c
William Pepple <williampepple1@gmail.com>
2025-04-25 19:32:23 +0100

    change name from russ to rusc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let css = russ::generate_css(); // From your lib.rs
+    let css = rusc::generate_css(); // From your lib.rs
     std::fs::create_dir_all("dist").unwrap();
     std::fs::write("dist/output.css", css).expect("Failed to write dist/output.css");
     println!("✅ dist/output.css generated!");
