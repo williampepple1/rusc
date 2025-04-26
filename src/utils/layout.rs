9d5212dc6470fa7d67df5297880bb1cae5e88b5b
William Pepple <williampepple1@gmail.com>
2025-04-26 14:49:24 +0100

    put margin and padding under layout module

--- src/utils/layout.rs
+++ src/utils/layout.rs
@@ -1,3 +1,40 @@
+pub fn generate_margin_utilities() -> String {
+    let spacings = vec![0, 1, 2, 3, 4, 5];
+    let mut css = String::new();
+
+    for val in spacings {
+        let px = val * 4;
+        css.push_str(&format!(".m-{} {{ margin: {}px; }}\n", px, px));
+        css.push_str(&format!(".mt-{} {{ margin-top: {}px; }}\n", px, px));
+        css.push_str(&format!(".mr-{} {{ margin-right: {}px; }}\n", px, px));
+        css.push_str(&format!(".mb-{} {{ margin-bottom: {}px; }}\n", px, px));
+        css.push_str(&format!(".ml-{} {{ margin-left: {}px; }}\n", px, px));
+        css.push_str(&format!(".mx-{} {{ margin-left: {}px; margin-right: {}px; }}\n", px, px, px));
+        css.push_str(&format!(".my-{} {{ margin-top: {}px; margin-bottom: {}px; }}\n", px, px, px));
+    }
+
+    css
+}
+
+
+pub fn generate_padding_utilities() -> String {
+    let spacings = vec![0, 1, 2, 3, 4, 5];
+    let mut css = String::new();
+
+    for val in spacings {
+        let px = val * 4;
+        css.push_str(&format!(".p-{} {{ padding: {}px; }}\n", px, px));
+        css.push_str(&format!(".pt-{} {{ padding-top: {}px; }}\n", px, px));
+        css.push_str(&format!(".pr-{} {{ padding-right: {}px; }}\n", px, px));
+        css.push_str(&format!(".pb-{} {{ padding-bottom: {}px; }}\n", px, px));
+        css.push_str(&format!(".pl-{} {{ padding-left: {}px; }}\n", px, px));
+        css.push_str(&format!(".px-{} {{ padding-left: {}px; padding-right: {}px; }}\n", px, px, px));
+        css.push_str(&format!(".py-{} {{ padding-top: {}px; padding-bottom: {}px; }}\n", px, px, px));
+    }
+
+    css
+}
+
 pub fn generate_height_utilities() -> String {
     let heights = vec![0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 16, 20, 24, 32, 40, 48, 56, 64];
     let mut css = String::new();
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,4 @@
 pub mod bg;
 pub mod text;
-pub mod padding;
-pub mod margin;
 pub mod font;
 pub mod layout;
